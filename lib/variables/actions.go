139095607e3c51fa57c6b6192cedab31d7743f2a
alagha-go <abdihakimosman1480@gmail.com>
2022-07-20 19:22:13 +0000

    deleted actions.go file

--- lib/variables/actions.go
+++
@@ -1,9 +0,0 @@
-package variables
-
-
-var (
-	InternalServerError = "internal server error"
-	CreateDepartment = "create department"
-	DepartMentExists = "department with the title %s exists"
-	DepartmentNotFound = "department with the id %s does not exist"
-)
