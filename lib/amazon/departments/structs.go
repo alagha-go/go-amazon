b9b693e1cede8ae0959f5c47ff18f12af19e9f3f
alagha-go <abdihakimosman1480@gmail.com>
2022-07-22 18:52:29 +0000

    changed the data structure

--- lib/amazon/departments/structs.go
+++ lib/amazon/departments/structs.go
@@ -2,36 +2,12 @@ package departments
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type Departments []Department
+
+
 type Department struct {
 	ID								*primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
 	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
 	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
-	Categories						[]Category										`json:"categories,omitempty" bson:"categories,omitempty"`
-}
-
-type Category struct {
-	ID								*primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
-	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
-	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
-	SubCategories					[]SubCategory									`json:"subcategories,omitempty" bson:"subcategories,omitempty"`
-}
-
-type SubCategory struct {
-	ID								*primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
-	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
-	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
-	Types							[]Type											`json:"types,omitempty" bson:"types,omitempty"`
-}
-
-type Type struct {
-	ID								*primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
-	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
-	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
-	SubTypes						[]SubType										`json:"subtypes,omitempty" bson:"subtypes,omitempty"`
-}
-
-type SubType struct {
-	ID								*primitive.ObjectID								`json:"_id,omitempty" bson:"_id,omitempty"`
-	Title							string											`json:"title,omitempty" bson:"title,omitempty"`
-	Url								string											`json:"url,omitempty" bson:"url,omitempty"`
+	Departments						[]Department									`json:"departments,omitempty" bson:"departments,omitempty"`
 }
